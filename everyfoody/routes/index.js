94238e370d14fe05297584498c8939433a287e73
YeonTaeKIM <urbanscenery@gmail.com>
2017-10-06 03:34:49 +0900

    reservation/lists done

--- everyfoody/routes/index.js
+++ everyfoody/routes/index.js
@@ -6,15 +6,17 @@ var region = require('./main/index');
 var review = require('./review/index');
 var signin = require('./login/signin');
 var signup = require('./login/signup');
-//var reservation = require('./reservation/index');
+var reservation = require('./reservation/index');
 var api = require('./apireference');
 
 router.use('/store',store);
 router.use('/main', region);
 router.use('/bookmark', bookmark);
+router.use('/reservation', reservation);
 router.use('/review', review);
 router.use('/signin', signin);
 router.use('/signup', signup)
 router.use('/api', api);
+
 
 module.exports = router;
--- everyfoody/routes/reservation/index.js
+++ everyfoody/routes/reservation/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var reservation = require('./reservation');
+var lists = require('./list');
 
 
 router.use('/reservation', reservation);
+router.use('/lists', lists);
 
 
 module.exports = router;
