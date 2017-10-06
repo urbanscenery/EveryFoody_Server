e33ccbebb8a27d00277162c8646f82e860d9f079
YeonTaeKIM <urbanscenery@gmail.com>
2017-10-07 06:16:43 +0900

    review/lists done
    리뷰리스트 불러오기 완료

--- everyfoody/routes/review/index.js
+++ everyfoody/routes/review/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var registration = require('./registration');
+var lists = require('./lists');
 
 
 router.use('/registration', registration);
+router.use('/lists', lists);
 
 
 module.exports = router;
