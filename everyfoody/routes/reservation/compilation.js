85c979a810d542bd8c266b1c7388417d778e4f81
YeonTaeKIM <urbanscenery@gmail.com>
2017-10-07 07:22:16 +0900

    reservation/comilation, review/registration done
    예약 등록/삭제, 리뷰 등록 완료

--- everyfoody/routes/reservation/compilation.js
+++ everyfoody/routes/reservation/compilation.js
@@ -1,20 +1,123 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const async = require('async');
+const router = express.Router();
+const pool = require('../../config/db_pool');
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
 
-/* GET users listing. */
-router.get('/list', function(req, res, next) {
-
-  var user_email = req.body.user_email;
-  var owner_email = req.body.owner_email;
-  var review_score = req.body.review_score;
-  var review_content = req.body.review_content;
-  var store_name = req.params.store_name;
-
-  res.status(200).send({
-    msg: "success",
-    status: 200,
-    data: ""
+router.get('/:storeID', function(req, res) {
+  let taskArray = [
+    //1. connection 설정
+    function(callback) {
+      pool.getConnection(function(err, connection) {
+        if (err) {
+          res.status(500).send({
+            status: "fail",
+            msg: "get connection error"
+          });
+          callback("getConnection error : " + err, null);
+        } else callback(null, connection);
+      });
+    },
+    //2. header의 token 값으로 user_email 받아옴.
+    function(connection, callback) {
+      let token = req.headers.token;
+      if (token === "nonLoginUser") {
+        let decoded = {
+          userEmail: "nonSignin",
+          userID: 0,
+          userCategory: 0
+        }
+        callback(null, decoded, connection);
+      } else {
+        jwt.verify(token, req.app.get('jwt-secret'), function(err, decoded) {
+          if (err) {
+            res.status(500).send({
+              msg: "user authorization error"
+            });
+            connection.release();
+            callback("JWT decoded err : " + err, null);
+          } else {
+            callback(null, decoded.userID, connection);
+            //decoded가 하나의 JSON 객체. 이안에 userEmail userCategory userID 프로퍼티 존
+          }
+        });
+      }
+    },
+    //3. userID, storeID로 예약내역 있는지 검사
+    function(userID, connection, callback) {
+      let selectReservationQuery = 'select * from reservation where user_id = ? and owner_id = ?';
+      connection.query(selectReservationQuery, [userID, req.params.storeID], function(err, reservationData) {
+        if (err) {
+          res.status(500).send({
+            status: "fail",
+            msg: "get reservation data error"
+          });
+          connection.release();
+          callback("get reservation data err : " + err, null);
+        } else {
+          callback(null, reservationData, userID, connection);
+        }
+      });
+    },
+    //4. reservation데이터가 있으면 삭제, 없으면 추가
+    function(reservationData, userID, connection, callback) {
+      if (reservationData.length === 0) {
+        let insertReservationQuery = 'insert into reservation set ?';
+        let reservationData = {
+          user_id : userID,
+          owner_id : req.params.storeID,
+          reservation_time : moment().format('YYYYMMDDhhmmss')
+        }
+        connection.query(insertReservationQuery, reservationData, function(err) {
+          if (err) {
+            res.status(500).send({
+              status: "fail",
+              msg: "insert reservation data error"
+            });
+            connection.release();
+            callback("insert reservation data err : " + err, null);
+          } else {
+            res.status(200).send({
+              status : "success",
+              msg : "successful regist reservation"
+            });
+            connection.release();
+            callback(null, "succesful regist reservation");
+          }
+        });
+      } else {
+        let deleteReservationQuery = 'delete from reservation where user_id = ? and owner_id = ?';
+        connection.query(deleteReservationQuery, [userID, req.params.storeID], function(err) {
+          if (err) {
+            res.status(500).send({
+              status: "fail",
+              msg: "delete reservation data error"
+            });
+            connection.release();
+            callback("delete reservation data err : " + err, null);
+          } else {
+            res.status(200).send({
+              status : "success",
+              msg : "successful delete reservation"
+            });
+            connection.release();
+            callback(null, "successful delete reservation");
+          }
+        });
+      }
+    }
+  ];
+  async.waterfall(taskArray, function(err, result) {
+    if (err) {
+      err = moment().format('MM/DDahh:mm:ss//') + err;
+      console.log(err);
+    } else {
+      result = moment().format('MM/DDahh:mm:ss//') + result;
+      console.log(result);
+    }
   });
-});
+})
 
 module.exports = router;
--- everyfoody/routes/review/registration.js
+++ everyfoody/routes/review/registration.js
@@ -1,20 +1,111 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/registration/:storeID', function(req, res, next) {
-
-  var user_email = req.body.user_email;
-  var owner_email = req.body.owner_email;
-  var review_score = req.body.review_score;
-  var review_content = req.body.review_content;
-  var store_name = req.params.store_name;
-
-  res.status(200).send({
-    msg: "success",
-    status: 200,
-    data: ""
-  });
+const express = require('express');
+const async = require('async');
+const router = express.Router();
+const pool = require('../../config/db_pool');
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const aws = require('aws-sdk');
+aws.config.loadFromPath('./config/aws_config.json');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const s3 = new aws.S3();
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'everyfoody',
+        acl: 'public-read',
+        key: function(req, file, cb) {
+            cb(null, Date.now() + '.' + file.originalname.split('.').pop());
+        }
+    })
 });
+
+router.post('/',upload.single('image') ,function(req, res) {
+  let taskArray = [
+    //1. connection 설정
+    function(callback) {
+      pool.getConnection(function(err, connection) {
+        if (err) {
+          res.status(500).send({
+            status: "fail",
+            msg: "get connection error"
+          });
+          callback("getConnection error : " + err, null);
+        } else callback(null, connection);
+      });
+    },
+    //2. header의 token 값으로 user_email 받아옴.
+    function(connection, callback) {
+      let token = req.headers.token;
+      if (token === "nonLoginUser") {
+        let decoded = {
+          userEmail: "nonSignin",
+          userID: 0,
+          userCategory: 0
+        }
+        callback(null, decoded.userID, connection);
+      } else {
+        jwt.verify(token, req.app.get('jwt-secret'), function(err, decoded) {
+          if (err) {
+            res.status(500).send({
+              msg: "user authorization error"
+            });
+            connection.release();
+            callback("JWT decoded err : " + err, null);
+          } else {
+            callback(null, decoded.userID, connection);
+            //decoded가 하나의 JSON 객체. 이안에 userEmail userCategory userID 프로퍼티 존
+          }
+        });
+      }
+    },
+    //3. 리뷰 등록
+    function(userID, connection, callback){
+      let insertReviewQuery = 'insert into reviewes set ?';
+      let imageURL;
+      if(typeof req.file === "undefined"){
+        imageURL = null;
+      }else{
+        imageURL = req.file.location;
+      }
+      let reviewData = {
+        user_id : userID,
+        owner_id : req.body.storeID*1,
+        review_score : (req.body.score)*1,
+        review_content : req.body.content,
+        review_imageURL : imageURL
+      };
+      console.log(reviewData);
+      connection.query(insertReviewQuery, reviewData, function(err){
+        if(err){
+          res.status(500).send({
+            status : "fail",
+            msg : "regist review error"
+          });
+          connection.release();
+          callback("regist review error : "+ err, null);
+        }
+        else{
+          res.status(201).send({
+            status : "success",
+            msg : "successful regist reivew"
+          });
+          connection.release();
+          callback(null, "successful regist review");
+        }
+      });
+    }
+  ];
+  async.waterfall(taskArray, function(err, result) {
+    if (err) {
+      err = moment().format('MM/DDahh:mm:ss//') + err;
+      console.log(err);
+    } else {
+      result = moment().format('MM/DDahh:mm:ss//') + result;
+      console.log(result);
+    }
+  });
+})
 
 module.exports = router;
