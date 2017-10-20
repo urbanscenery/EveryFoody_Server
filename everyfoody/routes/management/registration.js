2e24bd08b404791a3e954a62e41f650d796c99be
dongsudongsu <30032dongsu@gmail.com>
2017-10-20 15:33:04 +0900

    user_status modification

--- everyfoody/routes/management/registration.js
+++ everyfoody/routes/management/registration.js
@@ -7,10 +7,11 @@ const pool = require('../../config/db_pool')
 const upload = require('../../modules/AWS-S3');
 
 
-router.put('/store', upload.single('image'), function(req, res, next) {
+router.post('/store', upload.single('image'), function(req, res, next) {
 
-	var authURL = req.file.location;
+	var authURL = 'serser';
 	var store_name = req.body.store_name;
+
 	let taskArray = [
 		function(callback){
 			pool.getConnection(function(err, connection) {
@@ -33,7 +34,7 @@ router.put('/store', upload.single('image'), function(req, res, next) {
 	      })
     	},
 		function(owner_id, connection, callback) {
-			let setOwnerQuery = 'insert into owner(owner_id, owner_storename, owner_authURL) values (?, ?, ?)';
+			let setOwnerQuery = 'insert into owners (owner_id, owner_storename, owner_authURL) values (?, ?, ?)';
 			connection.query(setOwnerQuery, [owner_id, store_name, authURL], function(err) {
 				if(err) {
 					res.status(501).send({
@@ -43,13 +44,27 @@ router.put('/store', upload.single('image'), function(req, res, next) {
 					connection.release();
 					callback("update info error :"+ err,null);
 				}
-				else {
+				else callback(null, owner_id, connection);
+			});
+		},
+		function(owner_id, connection, callback) {
+			let changeCategoryQuery = "UPDATE users set user_status = ? where user_id = ?";
+			connection.query(changeCategoryQuery,[403, owner_id], function(err){
+				if(err) {
+					res.status(501).send({
+						status: "fail",
+						msg : "update query error"
+					});
+					connection.release();
+					callback("update query error : "+ err, null);			
+				}
+				else{
 					res.status(200).send({
 						status : "success",
 						msg : "Success"
 					});
 					connection.release();
-					callback(null, "Successful regist owner info");
+					callback(null, "successful update user category");
 				}
 			});
 		}
@@ -99,7 +114,7 @@ router.put('/closing', function(req, res, next) {
 	      })
     	},
 		function(owner_id, connection, callback) {
-			let setLocationQuery = 'UPDATE owner SET owner_latitude = ?, owner_longitude = ? where owner_id = ?';
+			let setLocationQuery = 'UPDATE owners SET owner_latitude = ?, owner_longitude = ? where owner_id = ?';
 			connection.query(setLocationQuery, [opentruck_latitude, opentruck_longitude, owner_id], function(err) {
 				if(err) {
 					res.status(500).send({
@@ -164,7 +179,7 @@ router.put('/opening', function(req, res, next) {
 	      })
     	},
 		function(owner_id, connection, callback) {
-			let setLocationQuery = 'UPDATE owner SET owner_latitude = ?, owner_longitude = ? where owner_id = ?';
+			let setLocationQuery = 'UPDATE owners SET owner_latitude = ?, owner_longitude = ? where owner_id = ?';
 			connection.query(setLocationQuery, [opentruck_latitude, opentruck_longitude, owner_id], function(err) {
 				if(err) {
 					res.status(500).send({
