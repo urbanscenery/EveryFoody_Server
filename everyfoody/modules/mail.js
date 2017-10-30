6a2aaf6ec4bff0729643486bb77973f0758ee596
YeonTaeKIM <urbanscenery@gmail.com>
2017-10-31 03:54:58 +0900

    Mail Done

---
+++ everyfoody/modules/mail.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+module.exports.transport = nodemailer.createTransport({  
+    service: 'gmail',
+    auth: {
+        user: 'urbanscenery@gmail.com',
+        pass: 'password'
+    }
+});
+
+module.exports.option = {  
+    from: 'urbanscenery@gmail.com',
+    to: 'urbanscenery@gmail.com',
+    subject: '에브리푸디 사업자 인증요청',
+    html:''
+};
+
+
+module.exports.html1 = '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body><div style="margin : 20px auto; border: 1px solid #cccccc; width:500px;">'+
+		'<div class="title" style="font-size: 30px; text-align: center; margin-top:15px">'+
+		'사업자 등록 인증 메일입니다.</div><br><img src="';
+module.exports.html2 = '"/><div style="text-align: center">인증을 받으시려면 <a href="220.230.114.8:3444/admin/comfirm/';
+module.exports.html3 =	'">"인증하기"</a>를 눌러주세요.<br></div><div style="text-align: center; margin-bottom: 15px;">'+
+		'인증을 거절하시려면<a href="220.230.114.8:3444/admin/reject/';
+module.exports.html4 = '">"거절하기"</a>를 눌러주세요.</div></div></body></html>'
+
---
+++ everyfoody/routes/admin/reject.js
@@ -0,0 +1,81 @@
+const async = require('async');
+const mysql = require('mysql');
+const moment = require('moment');
+const pool = require('../../config/db_pool');
+const express = require('express');
+const router = express.Router();
+
+router.get('/:ownerID', function(req, res) {
+
+  let taskArray = [
+    function(callback) {
+      pool.getConnection(function(err, connection) {
+        if (err) callback("DB connection error :" + err, null);
+        else callback(null, connection)
+      });
+    },
+    function(connection, callback) {
+      let updateOwnerQuery = 'update users set user_status = ? where user_id = ?';
+      connection.query(updateOwnerQuery, [404, req.params.ownerID], (err) => {
+        if (err) {
+          connection.release();
+          callback("update owner status query error : " + err, null);
+        } else {
+          callback(null, connection, "successful update owner status");
+        }
+      });
+    },
+    function(connection, successMSG, callback){
+      let deleteTruckQuery = 'delete from owners where owner_id = ?';
+      connection.query(deleteTruckQuery, req.params.ownerID, function(err){
+        if(err){
+          connection.release();
+          callback(successMSG + " // delete truck query error : "+ err, null);
+        } else{
+          callback(null, connection, successMSG + " // delete done ");
+        }
+      })
+    },
+    function(connection, successMSG, callback) {
+      let selectDeviceTokenQuery = 'select user_deviceToken from users where user_id = ?';
+      connection.query(selectDeviceTokenQuery, req.params.ownerID, function(err, token) {
+        if (err) {
+          connection.release();
+          callback('select device token query error : ' + err, null);
+        } else {
+          callback(null, connection, successMSG, token[0].user_deviceToken);
+        }
+      });
+    },
+    function(connection, successMSG, deviceToken, callback) {
+      let message = {
+        to: deviceToken,
+        collapse_key: 'Updates Available',
+        data: {
+          title: "가게 인증 실패 알림",
+          body: "가게 인증이 거절되었습니다. 영업허가증 확인후 다시 등록해주세요."
+        }
+      };
+      fcm.send(message, function(err, response) {
+        if (err) {
+          connection.release();
+          callback(successMsg + " // send push msg error : " + err, null);
+        } else {
+          connection.release();
+          callback(null, successMsg + " // success send push msg : " + response);
+        }
+      });
+    }
+  ];
+  async.waterfall(taskArray, function(err, result) {
+    if (err) {
+      err = moment().format('MM/DDahh:mm:ss// ') + err;
+      console.log(err);
+    } else {
+      result = moment().format('MM/DDahh:mm:ss// ') + result;
+      console.log(result);
+    }
+  });
+});
+
+module.exports = router;
